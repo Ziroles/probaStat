c22cdc95a8b7841f8e3f09d8c47c0ff8074c2748
Nathan FERRY <nathan.ferry@live.fr>
2025-02-18 10:07:25 +0100

    Reformat filename

--- class/DataManager.py
+++ class/DataManager.py
@@ -1,19 +1,23 @@
 import pandas as pd
 
+
 class DataManager:
 
-
-    
     def __init__(self):
         self.data = {}
-        self.fileName = {"olistCustomersDataset":"olist_customers_dataset.csv", "olistGeolocationDataset":"olist_geolocation_dataset.csv", "olistOrderItemsDataset":"olist_order_items_dataset.csv", "olistOrderPaymentsDataset":"olist_order_payments_dataset.csv","olistOrderReviewsDataset": "olist_order_reviews_dataset.csv","olistOrdersDataset": "olist_orders_dataset.csv","olistProductsDataset": "olist_products_dataset.csv", "olistSellersDataset":"olist_sellers_dataset.csv", "productCategoryNameTranslation":"product_category_name_translation.csv"}
+        self.fileName = {"Customers": "olist_customers_dataset.csv",
+                         "Geolocation": "olist_geolocation_dataset.csv",
+                         "OrderItems": "olist_order_items_dataset.csv",
+                         "OrderPayments": "olist_order_payments_dataset.csv",
+                         "OrderReviews": "olist_order_reviews_dataset.csv",
+                         "Orders": "olist_orders_dataset.csv",
+                         "Products": "olist_products_dataset.csv",
+                         "Sellers": "olist_sellers_dataset.csv",
+                         "ProductCategoryNameTranslation": "product_category_name_translation.csv"}
         self.load_data()
-        
 
-    def data(self,key):
+    def data(self, key):
         return self.data[key]
-    
-    
 
     def load_data(self):
         for i in self.fileName:
