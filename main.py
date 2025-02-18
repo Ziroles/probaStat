281aa6accd99ac6c2ed0fc94d9ba78c60f873542
Nathan FERRY <nathan.ferry@live.fr>
2025-02-18 12:37:21 +0100

    Working merging

--- main.py
+++ main.py
@@ -4,3 +4,10 @@ from utils.DataManager import DataManager
 
 if __name__ == "__main__":
     dataManager = DataManager()
+    customers = dataManager.get_dataframe("Customers")
+    orders = dataManager.get_dataframe("Orders")
+    orderReviews = dataManager.get_dataframe("OrderReviews")
+
+    reviewsByOrder = pd.merge(orders, orderReviews, how="inner", on="order_id")
+
+    print(reviewsByOrder)
