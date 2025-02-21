501606b714cdaefe46b4bbe40d966b3ebafdb072
Nathan FERRY <nathan.ferry@live.fr>
2025-02-21 14:19:48 +0100

    customer by state

--- main.py
+++ main.py
@@ -5,9 +5,5 @@ from utils.DataManager import DataManager
 if __name__ == "__main__":
     dataManager = DataManager()
     customers = dataManager.get_dataframe("Customers")
-    orders = dataManager.get_dataframe("Orders")
-    orderReviews = dataManager.get_dataframe("OrderReviews")
 
-    reviewsByOrder = pd.merge(orders, orderReviews, how="inner", on="order_id")
-
-    print(reviewsByOrder)
+    print(customers.groupby('customer_state')['customer_id'])
